677fe189d98fffed7e9e8666e96b7dbf08371ec1
Paul Montgomery <pamontg@microsoft.com>
2020-07-15 11:40:00 -0700

    Moving namespaces

---
+++ azext_iot/product/params.py
@@ -0,0 +1,149 @@
+# coding=utf-8
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+"""
+CLI parameter definitions.
+"""
+
+from azure.cli.core.commands.parameters import get_three_state_flag, get_enum_type
+from azext_iot.product.shared import AuthType, BadgeType, TaskType
+
+
+def load_product_params(self, _):
+    with self.argument_context('iot product') as c:
+        c.argument(
+            'product_name',
+            options_list=['--product-name'],
+            help='Product name to display in catalog',
+            arg_group='IoT Device Certification'
+        )
+        c.argument(
+            'test_id',
+            options_list=['--test-id'],
+            help='The generated Id for the device certification test',
+            arg_group='IoT Device Certification'
+        )
+        c.argument(
+            'badge_type',
+            options_list=['--badge-type', '--bt'],
+            help='The type of certification badge',
+            arg_group='IoT Device Certification',
+            arg_type=get_enum_type(BadgeType)
+        )
+    with self.argument_context('iot product test') as c:
+        c.argument(
+            'working_folder',
+            options_list=['--working-folder', '--wf'],
+            help='The folder to create in current path',
+            arg_group='IoT Device Certification'
+        )
+        c.argument(
+            'auth_type',
+            options_list=['--authentication-type', '--at'],
+            help='How the device will authenticate to testing service Device Provisioning Service',
+            arg_group='IoT Device Certification',
+            arg_type=get_enum_type(AuthType)
+        )
+        c.argument(
+            'provisioning',
+            options_list=['--provisioning'],
+            help='Determines whether the service generates provisioning configuration. '
+            + 'Only applies to SymmetricKey and ConnectionString provisioning types',
+            arg_group='IoT Device Certification'
+        )
+        c.argument(
+            'configuration_file',
+            options_list=['--configuration-file', '--cf'],
+            help='Path to device test JSON file',
+            arg_group='IoT Device Certification'
+        )
+    with self.argument_context('iot product test search') as c:
+        c.argument(
+            'product_id',
+            options_list=['--product-id', '--pi'],
+            help='The submitted product id',
+            arg_group='IoT Device Certification'
+        )
+        c.argument(
+            'registration_id',
+            options_list=['--registration-id', '--ri'],
+            help='The regstration Id for Device Provisioning Service',
+            arg_group='IoT Device Certification'
+        )
+        c.argument(
+            'certificate_name',
+            options_list=['--certificate-name', '--cn'],
+            help='The x509 Certificate Common Name (cn) used for Device Provisioning Service authentication',
+            arg_group='IoT Device Certification'
+        )
+    with self.argument_context('iot product test case') as c:
+        c.argument(
+            'configuration_file',
+            options_list=['--configuration-file', '--cf'],
+            help='The file path for test case configuration JSON',
+            arg_group='IoT Device Certification'
+        )
+    with self.argument_context('iot product test task') as c:
+        c.argument(
+            'task_id',
+            options_list=['--task-id'],
+            help='The generated Id of the testing task',
+            arg_group='IoT Device Certification'
+        )
+        c.argument(
+            'running',
+            options_list=['--running'],
+            help='Get the running tasks of a device test',
+            arg_group='IoT Device Certification',
+            arg_type=get_three_state_flag()
+        )
+        c.argument(
+            'type',
+            options_list=['--type', '-t'],
+            help='The type of task for the device test',
+            arg_group='IoT Device Certification',
+            arg_type=get_enum_type(TaskType)
+        )
+        c.argument(
+            'monitor',
+            options_list=['--monitor', '-m'],
+            help='Monitor task to completion and return test case data when available',
+            arg_group='IoT Device Certification',
+            arg_type=get_three_state_flag()
+        )
+        c.argument(
+            'monitor_interval',
+            options_list=['--monitor-interval', '--mi'],
+            help='Used in conjunction with --monitor. Sepcifies how frequently (in seconds) polling occurs',
+            arg_group='IoT Device Certification',
+        )
+    with self.argument_context('iot product test run') as c:
+        c.argument(
+            'run_id',
+            options_list=['--run-id'],
+            help='The generated Id of a test run',
+            arg_group='IoT Device Certification'
+        )
+        c.argument(
+            'monitor',
+            options_list=['--monitor', '-m'],
+            help='Monitor test run until status is "started" or "running"',
+            arg_group='IoT Device Certification',
+            arg_type=get_three_state_flag()
+        )
+        c.argument(
+            'monitor_interval',
+            options_list=['--monitor-interval', '--mi'],
+            help='Used in conjunction with --monitor. Sepcifies how frequently (in seconds) polling occurs',
+            arg_group='IoT Device Certification',
+        )
+        c.argument(
+            'latest',
+            options_list=['--latest'],
+            help='Retrieve the latest test runs',
+            arg_group='IoT Device Certification',
+            arg_type=get_three_state_flag()
+        )
